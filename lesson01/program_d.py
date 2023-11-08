20913bebe7a1aaa5ada6d0cca65df3c3c400a7aa
nomilious <eduard.belic.2002@gmail.com>
2023-11-08 15:12:10 +0300

    D. Сортировка слиянием

--- lesson01/program_d.py
+++ lesson01/program_d.py
@@ -3,7 +3,7 @@ def merge(slice1, slice2):
     ind1 = ind2 = 0
     n, m = len(slice1), len(slice2)
     while ind1 < n or ind2 < m:
-        if ind2 >= m or (ind1< n and  slice1[ind1] <= slice2[ind2]):
+        if ind2 >= m or (ind1 < n and  slice1[ind1] <= slice2[ind2]):
             result.append(slice1[ind1])
             ind1 += 1
         elif ind1 >= n or (ind2 < m and slice1[ind1] > slice2[ind2]):
@@ -11,12 +11,24 @@ def merge(slice1, slice2):
             ind2 += 1
     return result
 
-def sort():
-    pass
+def sort(array):
+    if len(array) <= 1:
+        return array
+    n = len(array)
+    k = round(n / 2)
+    # mergeSort left
+    slice1 = sort(array[:k])
+    # mergeSort right
+    slice2 = sort(array[k:])
+    
+    return merge(slice1, slice2)
+    
 
 def main():
     input()
     arr = list(map(int, input().split()))
+    res = sort(arr)
+    print(*res)
 
 
 if __name__ == "__main__":
